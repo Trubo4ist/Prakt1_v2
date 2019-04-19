d59181a551b3264a8482582042527a773db02dca
Trubo4ist <applepi2001@mail.ru>
2019-04-19 15:27:21 +0300

    Commit class only

--- Prakt1/Prakt1/ClassIzd.cs
+++ Prakt1/Prakt1/ClassIzd.cs
@@ -8,5 +8,13 @@ namespace Prakt1
 {
     class ClassIzd
     {
+        public string Name;
+        public string Shifr;
+        public int Count;
+        public ClassIzd(string n, string n1, int n2) { Name = n; Shifr = n1; Count = n2; }
+        public void GetInfo()
+        {
+            Console.WriteLine($"Название: {Name} Шифр: {Shifr} Количество: {Count} ");
+        }
     }
 }
